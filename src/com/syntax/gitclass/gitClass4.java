ee10677aa993097770a8572b5e13ce6eca09204a
KhrystynaKrytsyak <krytsyak@gmail.com>
2022-06-02 12:21:02 -0400

    modified the git class4

--- src/com/syntax/gitclass/gitClass4.java
+++ src/com/syntax/gitclass/gitClass4.java
@@ -4,7 +4,8 @@ public class gitClass4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("popculture");
+		System.out.println("abrakadabra");
 	}
 
 }
